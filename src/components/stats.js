46880ed12f7d8c923584f906d41b16aa1ffe9b2d
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-12-27 13:33:55 +0300

    add stats component

--- src/components/stats.js
+++ src/components/stats.js
@@ -1,6 +1,47 @@
 import React from "react";
 import { StatsSummary } from "./statsComponents/statsSummary";
+import { StatsRank } from "./statsComponents/statsRank";
+import { StatsFilters } from "./statsComponents/statsFilters";
+import {
+  getTopGenre,
+  getWatchedFilms,
+  getHoursAndMins,
+  getStatsRank,
+  getFilmsByFilter
+} from "../utils";
 
-export const Stats = ({ films }) => {
-  return <section className="statistic">{/*<StatsSummary />*/}</section>;
-};
+export class Stats extends React.Component {
+  constructor(props) {
+    super(props);
+    this.films = props.films;
+  }
+  state = { filterType: "all-time" };
+
+  onChangeFilter = filterType => {
+    this.setState({ filterType });
+  };
+  render() {
+    const films = this.films;
+    const filteredFilms = getFilmsByFilter(films, this.state.filterType);
+    const topGenre = getTopGenre(filteredFilms);
+    const watchedFilms = getWatchedFilms(filteredFilms);
+    const [hours, minutes] = getHoursAndMins(filteredFilms);
+    const statsRank = getStatsRank(filteredFilms.length);
+
+    return (
+      <section className="statistic">
+        // TODO: add active (checked) to filter tabs
+        <StatsRank rank={statsRank} />
+        <StatsFilters onChangeFilter={this.onChangeFilter} />
+        {
+          <StatsSummary
+            watched={watchedFilms}
+            hours={hours}
+            minutes={minutes}
+            topGenre={topGenre}
+          />
+        }
+      </section>
+    );
+  }
+}
