78ba505e70247b1a842a1902aa3579d68f862547
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-12-27 12:41:44 +0300

    add add statsSummary component

--- src/components/statsComponents/statsSummary.js
+++ src/components/statsComponents/statsSummary.js
@@ -6,21 +6,21 @@ export const StatsSummary = ({ watched, hours, minutes, topGenre }) => {
       <li className="statistic__text-item">
         <h4 className="statistic__item-title">You watched</h4>
         <p className="statistic__item-text">
-          ${watched}
+          {watched}
           <span className="statistic__item-description">movies</span>
         </p>
       </li>
       <li className="statistic__text-item">
         <h4 className="statistic__item-title">Total duration</h4>
         <p className="statistic__item-text">
-          ${hours}
-          <span className="statistic__item-description">h</span> ${minutes}
+          {hours}
+          <span className="statistic__item-description">h</span> {minutes}
           <span className="statistic__item-description">m</span>
         </p>
       </li>
       <li className="statistic__text-item">
         <h4 className="statistic__item-title">Top genre</h4>
-        <p className="statistic__item-text">${topGenre}</p>
+        <p className="statistic__item-text">{topGenre}</p>
       </li>
     </ul>
   );
