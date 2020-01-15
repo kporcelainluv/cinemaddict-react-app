8ec69a65ff76dc2c5a759cb9f89fca5d1501727f
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-01-15 14:06:28 +0300

    add fetch to a separate file

---
+++ src/api.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { AUTHORIZATION, END_POINT } from "./consts";
+
+export const getComment = (filmId, newComment) => {
+  return fetch(`${END_POINT}comments/${filmId}`, {
+    method: "POST",
+    headers: new Headers({
+      Authorization: AUTHORIZATION,
+      "Content-Type": "application/json"
+    }),
+    body: JSON.stringify(newComment)
+  });
+};
+
+export const deleteComment = commentId => {
+  return fetch(`${END_POINT}comments/${commentId}`, {
+    method: "DELETE",
+    headers: new Headers({
+      Authorization: AUTHORIZATION,
+      "Content-Type": "application/json"
+    })
+  });
+};
+
+export const getFilms = () => {
+  return fetch(`${END_POINT}movies`, {
+    method: "GET",
+    headers: new Headers({
+      Authorization: AUTHORIZATION,
+      "Content-Type": "application/json"
+    }),
+    body: null
+  });
+};
+export const getComments = filmId => {
+  return fetch(`${END_POINT}comments/${filmId}`, {
+    method: "GET",
+    headers: new Headers({
+      Authorization: AUTHORIZATION,
+      "Content-Type": "application/json"
+    }),
+    body: null
+  });
+};
